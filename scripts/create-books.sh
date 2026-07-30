531d636cc864690f1c3c48ef49a701ed26780d69
Matej Kubinec <matej.kubinec@outlook.com>
2026-07-31 00:53:05 +0200

    feat: added bass covers and small styling fixes

--- scripts/create-books.sh
+++ scripts/create-books.sh
@@ -3,7 +3,7 @@
 echo "30+30+01::started"
 mkdir -p books/30+30+01
 chordpro "--cover=songbooks/covers/30+30+01.pdf" "--filelist=songbooks/30+30+01.list" "--output=books/30+30+01/30+30+01.guitar.pdf" "--config=./config/settings.json"
-chordpro "--cover=songbooks/covers/30+30+01.pdf" "--filelist=songbooks/30+30+01.list" "--output=books/30+30+01/30+30+01.bass.pdf" "--decapo" "--config=./config/settings.json"
+chordpro "--cover=songbooks/covers/30+30+01.bass.pdf" "--filelist=songbooks/30+30+01.list" "--output=books/30+30+01/30+30+01.bass.pdf" "--decapo" "--config=./config/settings.json"
 echo "30+30+01::completed"
 
 echo "wedding::started"
@@ -15,5 +15,5 @@ echo "wedding::completed"
 echo "masterbook:started"
 mkdir -p books/masterbook
 chordpro "--cover=songbooks/covers/masterbook.pdf" songs/**/*.cho "--output=books/masterbook/masterbook.guitar.pdf" "--config=./config/settings.json"
-chordpro "--cover=songbooks/covers/masterbook.pdf" songs/**/*.cho "--output=books/masterbook/masterbook.bass.pdf" "--decapo" "--config=./config/settings.json"
+chordpro "--cover=songbooks/covers/masterbook.bass.pdf" songs/**/*.cho "--output=books/masterbook/masterbook.bass.pdf" "--decapo" "--config=./config/settings.json"
 echo "masterbook:completed"
